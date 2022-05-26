4b2496453fc91806aa0dd4b416b0772974719b44
Valeriia <volikavs@gmail.com>
2022-05-26 16:09:12 +0300

    update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredient = document.querySelector('#ingredients');
+
+const ingredientEl = ingredients.map(element => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = element;
+  ingredientItem.classList.add("item");
+  return ingredientItem;
+});
+
+console.log(ingredientEl);
+
+listIngredient.append(...ingredientEl);
