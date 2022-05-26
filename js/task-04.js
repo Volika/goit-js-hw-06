07d4a1357cc1eb40706f653fe8f5f5e5aedcd693
Valeriia <volikavs@gmail.com>
2022-05-26 23:28:33 +0300

    up 04

--- js/task-04.js
+++ js/task-04.js
@@ -3,15 +3,17 @@ const valeuEl = document.querySelector('#value');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 
-decrementBtn.addEventListener('click', () => {
-    counterValue = Number(valeuEl.textContent) - 1;
-    updateDisplay(counterValue);
-});
-incrementBtn.addEventListener('click', () => {
+decrementBtn.addEventListener('click', onDecrementBtnClick);
+incrementBtn.addEventListener('click', onIncrementBtnClick);
+
+function onIncrementBtnClick() {
     counterValue = Number(valeuEl.textContent) + 1;
     updateDisplay(counterValue);
-});
-
+}
+function onDecrementBtnClick() {
+    counterValue = Number(valeuEl.textContent) - 1;
+    updateDisplay(counterValue);
+}
 function updateDisplay(val) {
     valeuEl.innerHTML = val;
 }
