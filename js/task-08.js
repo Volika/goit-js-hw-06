c42943de5f7307d582d443ea71564dba657d5bc8
Valeriia <volikavs@gmail.com>
2022-05-27 14:43:55 +0300

    up 08

--- js/task-08.js
+++ js/task-08.js
@@ -2,7 +2,27 @@ const formRef = document.querySelector('.login-form');
 formRef.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-    event.prventDefault();
-
-    console.log('form submit');
+    event.preventDefault();
+    const newRegisr = {};
+    const formData = new FormData(event.currentTarget);
+         const {
+    elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+    return window.alert("Bсі поля повинні бути заповнені!");
+    } 
+    formData.forEach((value, name) => {
+        
+        newRegisr[name] = value; 
+            
+    });
+    
+    console.log(newRegisr);
+    event.currentTarget.reset();
+  
+   
+    // console.log(`Email: ${email.value}, Password: ${password.value}`);
+    
+    // return newRegisr;
 }
