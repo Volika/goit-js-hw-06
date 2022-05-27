a31f1d6f1b1aab2e4172f1bddb2f1f7a09001737
Valeriia <volikavs@gmail.com>
2022-05-27 12:58:34 +0300

    up 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,8 @@
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.prventDefault();
+
+    console.log('form submit');
+}
