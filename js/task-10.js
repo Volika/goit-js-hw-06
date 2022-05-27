f91d7a4301d1208d4aaee676b3d0314554f7e795
Valeriia <volikavs@gmail.com>
2022-05-27 16:45:49 +0300

    up 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputRef = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+const collectionSize = 0;
+
+inputRef.addEventListener('input', onInputChange);
+createBtn.addEventListener('click', onCreateBtnClick);
+destroyBtn.addEventListener('click', onDestroyBtnClick);
+
+function onCreateBtnClick() {
+   createOneElement()
+}
+function onDestroyBtnClick() {
+
+}
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  console.log(typeof event.currentTarget.value);
+  // collectionSize = Number(event.currentTarget.value);
+}
+
+function createOneElement() {
+  const newItem = document.createElement('div');
+  const newBgColor = getRandomHexColor();
+  newItem.style.backgroundColor = newBgColor;
+  
+  console.log(newItem);
+
+}
