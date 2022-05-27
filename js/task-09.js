f418288f9aa547bb47bfc85e72ced23f4afbd9a2
Valeriia <volikavs@gmail.com>
2022-05-27 15:30:27 +0300

    up 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const spanRef = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  const newBgColor = getRandomHexColor();
+  spanRef.innerHTML = newBgColor;
+
+  changeBackground(newBgColor);
+}
+
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
