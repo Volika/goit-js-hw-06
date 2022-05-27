2ee61249d7d6ba9a0de5a572aa7242a79b84e8ca
Valeriia <volikavs@gmail.com>
2022-05-27 17:35:32 +0300

    up 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputRef = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+const firstBoxSize = 30;
+let collectionSize =0;
+
+inputRef.addEventListener('input', onInputChange);
+createBtn.addEventListener('click', onCreateBtnClick);
+destroyBtn.addEventListener('click', onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  console.log(collectionSize);
+  const oneBoxSize = firstBoxSize;
+  const boxCollection = createOneElement(oneBoxSize);
+
+  boxesRef.append(boxCollection);
+}
+function onDestroyBtnClick() {
+
+}
+function onInputChange(event) {
+  // console.log(event.currentTarget.value);
+  // console.log(typeof event.currentTarget.value);
+  collectionSize = (event.currentTarget.value);
+}
+
+function createOneElement(value) {
+  const newItem = document.createElement('div');
+  const newBgColor = getRandomHexColor();
+  newItem.style.backgroundColor = newBgColor;
+  newItem.setAttribute('width', value);
+  newItem.setAttribute('height', value);
+  console.log(newItem);
+  return newItem;
+}
